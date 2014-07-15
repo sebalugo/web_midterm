9966ccc16b9390dfb909442ac83b92ef8f1d5606
Sebastian Lugo <sebalugo@ubuntu.(none)>
2014-07-15 10:20:27 -0400

    Added associations and amount of active subjects

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,10 @@
 class Teacher < ActiveRecord::Base
+
+	has_many :subjects
+	accepts_nested_attributes_for :subjects
+
+	def active_subjects
+		subjects.select{|x| x.active == "active"}.size
+	end
+
 end
