346289418248728de904330a190915ec7fe13620
Sebastian Lugo <sebalugo@ubuntu.(none)>
2014-07-15 11:22:05 -0400

    Added validations for attributes

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,4 @@
 class Subject < ActiveRecord::Base
 	belongs_to :teacher , dependent: :destroy
+	validates :active, inclusion: { in: ["active", "inactive"] }
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,7 +1,10 @@
 class Teacher < ActiveRecord::Base
 
+	validates :name , presence: true
+	validates :identity , presence: true , uniqueness: true
+
 	has_many :subjects
-	accepts_nested_attributes_for :subjects , reject_if: proc { |attributes| attributes['name'].blank? }
+	accepts_nested_attributes_for :subjects , reject_if: proc { |attributes| attributes['name'].blank? || attributes['identity'].blank? || attributes['active'].blank? }
 
 	def active_subjects
 		subjects.select{|x| x.active == "active"}.size
