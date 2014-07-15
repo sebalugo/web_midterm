725acf9515b00388805affe779bcc8bdfbb35832
Sebastian Lugo <sebalugo@ubuntu.(none)>
2014-07-15 10:36:55 -0400

    Added attributes to teacher show

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,7 +1,7 @@
 class Teacher < ActiveRecord::Base
 
 	has_many :subjects
-	accepts_nested_attributes_for :subjects
+	accepts_nested_attributes_for :subjects , reject_if: proc { |attributes| attributes['name'].blank? }
 
 	def active_subjects
 		subjects.select{|x| x.active == "active"}.size
